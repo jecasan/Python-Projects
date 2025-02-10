de5b751b6b7593a0aa4e323ab7ddd2c1e2c77bd9
jecasan <166345926+jecasan@users.noreply.github.com>
2025-02-10 11:27:45 +0800

    Update main.py

--- States/main.py
+++ States/main.py
@@ -14,13 +14,14 @@ ALIGNMENT = "center"
 FONT = ("Courier", 12, "normal")
 
 
-""" 
-Get coordinates on map
-def get_mouse_click_coor(x, y):
-    print(x,y)
+ 
+# # Get coordinates on map
+# def get_mouse_click_coor(x, y):
+#     print(x,y)
     
-screen.onscreenclick(get_mouse_click_coor)
-""" 
+# screen.onscreenclick(get_mouse_click_coor)
+# screen.mainloop()
+
 data = pandas.read_csv("50_states.csv")
 
 states_list = data.state.tolist()
@@ -30,10 +31,7 @@ while len(guessed_state) != 50:
     answer_state = screen.textinput(title = f"{len(guessed_state)}/50 States Correct", 
                                     prompt = "What's another state's name?").title()
     if answer_state == "Exit":
-        not_guessed = []
-        for state in states_list:
-            if state not in guessed_state:
-                not_guessed.append(state)
+        not_guessed = [state for state in states_list if state not in guessed_state]
         states_to_learn = pandas.DataFrame(not_guessed)
         states_to_learn.to_csv("states_to_learn.csv")
         break
@@ -43,9 +41,4 @@ while len(guessed_state) != 50:
         writer.write(answer_state, align = ALIGNMENT, font = FONT)
         guessed_state.append(answer_state)
             
-        
     
-
-
-
-
