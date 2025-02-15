0becd75b473d73fd0a1a9d3d54427ae11550dda8
jecasan <166345926+jecasan@users.noreply.github.com>
2025-02-15 16:45:30 +0800

    Add files via upload
    Currently doesn't support multiple recipients

---
+++ birthday-wisher/main.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import datetime as dt
+import smtplib
+import random as rd
+
+MY_EMAIL = "jerickpython@gmail.com"
+PASSWORD = "ksopdjmxasbmbzxq"
+
+def main():
+    month, day = get_date()
+    bday_dict = birthday_data()
+    email, name = check_day(month, day, bday_dict)
+    message = pick_template(name)
+    send_email(email, message)
+#------------Get Date------------#
+def get_date():
+    now = dt.datetime.now()
+    return now.month, now.day
+#------------Read CSV------------#
+def birthday_data():
+    data = pd.read_csv("birthdays.csv")
+    bday_dict = data.to_dict(orient="records")
+    return bday_dict
+#------------Pick Letter------------#
+def pick_template(name):
+    rand_num = rd.randint(1, 10)
+    with open(f"letter_templates/letter_{rand_num}.txt") as file:
+        letter = file.read()
+        message = letter.replace("[NAME]", name)
+        return message
+#------------Check Day------------#
+def check_day(month, day, dict):   
+    for date in dict:
+        if month == date["month"] and day == date["day"]:
+            return date["email"], date["name"]
+#------------Send Email------------#
+def send_email(email, message):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, 
+                            to_addrs=email, 
+                            msg=f"Subject:Happy Birthday\n\n{message}".encode("windows-1252")
+        )
+        
+
+if __name__ == "__main__":
+    main()
