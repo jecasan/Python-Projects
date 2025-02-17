a3d6830e2d10fa702351fd0a26c30d8e21616474
jecasan <166345926+jecasan@users.noreply.github.com>
2025-02-17 16:12:06 +0800

    Update data.py
    Now able to get random set of question every time you run the program

--- QuizGame/data.py
+++ QuizGame/data.py
@@ -1,9 +1,16 @@
-## Open source to update questions: https://opentdb.com/
+import requests
+import random
 
-question_data = [{"type":"boolean","difficulty":"medium","category":"Science: Computers","question":"The common software-programming acronym 'I18N' comes from the term 'Interlocalization'.","correct_answer":"False","incorrect_answers":["True"]},
-                 {"type":"boolean","difficulty":"medium","category":"Entertainment: Music","question":"Metallica collaborated with Rowan Atkinson's Mr Bean on a 1992 comic relief single.","correct_answer":"False","incorrect_answers":["True"]},
-                 {"type":"boolean","difficulty":"medium","category":"Geography","question":"You could walk from Norway to North Korea while only passing through Russia.","correct_answer":"True","incorrect_answers":["False"]},
-                 {"type":"boolean","difficulty":"medium","category":"Entertainment: Video Games","question":"In 'Resident Evil', only Chris has access to the grenade launcher.","correct_answer":"False","incorrect_answers":["True"]},
-                 {"type":"boolean","difficulty":"medium","category":"Animals","question":"Tigers have one colour of skin despite the stripey fur.","correct_answer":"False","incorrect_answers":["True"]},
-                 {"type":"boolean","difficulty":"medium","category":"Entertainment: Video Games","question":"'Return to Castle Wolfenstein' was the only game of the Wolfenstein series where you don't play as William 'B.J.' Blazkowicz.","correct_answer":"False","incorrect_answers":["True"]},
-                 {"type":"boolean","difficulty":"medium","category":"Entertainment: Japanese Anime &amp; Manga","question":"The anime Attack on Titan was directed by Tetsur\u014d Araki, the same person who directed the anime Highschool of the Dead.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"Science &amp; Nature","question":"Like with the Neanderthals, Homo sapiens sapiens also interbred with the Denisovans.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"Entertainment: Television","question":"Klingons once had a period of Democracy in their history, they referred to it as the 'Dark Times'.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"Entertainment: Film","question":"There aren't any live-action clones in 'Star Wars: Episode III - Revenge of the Sith'.","correct_answer":"True","incorrect_answers":["False"]}]
+"""
+You may choose the difficulty or keep as random using random choice with DIFFICULTY list.
+You may add a category parameter choose the category from the range of 9 - 32 
+or keep as random using randint. However some categories do not support some difficulty stage.
+"""
+DIFFICULTY = ["easy", "medium", "hard"]
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+    "difficulty": random.choice(DIFFICULTY),
+}
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+question_data = response.json()["results"]
